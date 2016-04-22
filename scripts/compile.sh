36fef06150a2c9e12f6cba30e4935b6a7f3b24e5
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-04-22 17:57:47 +0300

    Fix paths again

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Copy config
-cp ../config/config.ini.php ../vendor/piwik/piwik/config/config.ini.php
+cp config/config.ini.php vendor/piwik/piwik/config/config.ini.php
 
 # Update config
 DB_HOST=`echo $DATABASE_URL | cut -d@ -f2 | cut -d: -f1`
@@ -9,15 +9,15 @@ DB_USERNAME=`echo $DATABASE_URL | cut -d@ -f1 | cut -d/ -f3 | cut -d: -f1`
 DB_PASSWORD=`echo $DATABASE_URL | cut -d@ -f1 | cut -d/ -f3 | cut -d: -f2`
 DB_NAME=`echo $DATABASE_URL | cut -d@ -f2 | cut -d: -f2 | cut -d/ -f2`
 
-sed -i s/#DB_HOST/$DB_HOST/ ../vendor/piwik/piwik/config/config.ini.php
-sed -i s/#DB_USERNAME/$DB_USERNAME/ ../vendor/piwik/piwik/config/config.ini.php
-sed -i s/#DB_PASSWORD/$DB_PASSWORD/ ../vendor/piwik/piwik/config/config.ini.php
-sed -i s/#DB_NAME/$DB_NAME/ ../vendor/piwik/piwik/config/config.ini.php
+sed -i s/#DB_HOST/$DB_HOST/ vendor/piwik/piwik/config/config.ini.php
+sed -i s/#DB_USERNAME/$DB_USERNAME/ vendor/piwik/piwik/config/config.ini.php
+sed -i s/#DB_PASSWORD/$DB_PASSWORD/ vendor/piwik/piwik/config/config.ini.php
+sed -i s/#DB_NAME/$DB_NAME/ vendor/piwik/piwik/config/config.ini.php
 
-sed -i s/#SECRET_TOKEN/$SECRET_TOKEN/ ../vendor/piwik/piwik/config/config.ini.php
-sed -i s/#POSTMARK_TOKEN/$POSTMARK_TOKEN/ ../vendor/piwik/piwik/config/config.ini.php
+sed -i s/#SECRET_TOKEN/$SECRET_TOKEN/ vendor/piwik/piwik/config/config.ini.php
+sed -i s/#POSTMARK_TOKEN/$POSTMARK_TOKEN/ vendor/piwik/piwik/config/config.ini.php
 
 # Download maxmind db
-curl http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz > ../vendor/piwik/piwik/misc/GeoLiteCity.dat.gz
-gunzip ../vendor/piwik/piwik/misc/GeoLiteCity.dat.gz
-mv ../vendor/piwik/piwik/misc/GeoLiteCity.dat ../vendor/piwik/piwik/misc/GeoIPCity.dat
+curl http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz > vendor/piwik/piwik/misc/GeoLiteCity.dat.gz
+gunzip vendor/piwik/piwik/misc/GeoLiteCity.dat.gz
+mv vendor/piwik/piwik/misc/GeoLiteCity.dat vendor/piwik/piwik/misc/GeoIPCity.dat
